60f755b9bb981313afaabfa2ef693d77e22505f6
haoxinluo <haoxin.l.stuy@gmail.com>
2013-09-24 20:12:26 -0400

    added new funct

--- 7/luo_haoxin/try2err.py
+++ 7/luo_haoxin/try2err.py
@@ -1,10 +1,14 @@
 #try to errrrrrrrrrrrrrrrrrr#
 
+def err():
+    print("HELLO WORLD!")
+
 def main():
     input = raw_input("hi! what is your name?")
     for n in input:
-        print("*********WARNING*************")
+        print("*********WARNING*************WARNING*********************")
     
+    err()
 
 if __name__ == "__main__":
     main()
