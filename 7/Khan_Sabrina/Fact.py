4bb1c1a11e65d1637e24f65727c61e330c5b4135
Sabrina Khan <sabrina.khan739@gmail.com>
2013-09-24 21:32:48 -0400

    Improvements

--- 7/Khan_Sabrina/Fact.py
+++ 7/Khan_Sabrina/Fact.py
@@ -10,3 +10,25 @@ def multiples(n):
 print multiples(10)
 print multiples(23)
 
+
+def isPrime(n):
+    a = 2;
+    while(a < n-1):
+        if(n % a == 0):
+            return False;
+        a = a + 1;
+    return True;
+
+
+def largestprime(n):
+    a = 0;
+    i = n;
+    if(i <= 3):
+        return i;
+    while(i>3):
+        if(n % i == 0 and isPrime(i)):
+            return i;
+        i = i - 1;
+
+print largestprime(13195)
+           
