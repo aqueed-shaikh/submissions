13b2c81aafcd914ef6f5c1719e5527415bfc24a5
Will <flyinbananatree@me.com>
2013-09-24 20:35:38 -0400

    hermanincarnate

--- 7/herman_hunter/hermanroar.py
+++ 7/herman_hunter/hermanroar.py
@@ -1,4 +1,5 @@
 import random
 
-for x in range(0, random.randrange(0, 999)):
-    print "I AM HERMAN HEAR ME ROAR"
+for x in range(0, random.randrange(1, 999)):
+    print "I AM HERMAN NUMBER %i HEAR ME ROAR"%(x*x)
+    
