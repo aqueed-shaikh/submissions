119f85f40b14b093551ff2819220589681400a84
Tke <tke@Fail.(none)>
2013-09-24 22:18:38 -0400

    ._.

--- 6/Ye_tyrone/hi.py
+++ 6/Ye_tyrone/hi.py
@@ -8,12 +8,20 @@ def home:
     here = here + 1
     temp = """
 <h1>Been here %i times </h1>
-<a href = "/out"> go out? </a>
+<a href = "/out"> Go out? </a>
 """
 
 @hoarx.route("/out")
 def out:
     return """
-<h1>You're out </h1>
-<a href = "/"> go back? </a>
+<h1>You're outside the door </h1>
+<a href = "/"> Go back? </a>
+<a href = "/out/explore">Explore?</a> 
+"""
+
+@hoarx.route("/out/explore")
+def outExplore:
+    return """
+<h3>Yousa Sleepy<h3>
+<a href = "/"> Go back? </a>
 """
