04ba85b787434a0be5f26b952f219bf1233266be
Hanson Lin <hanson@hanson-VirtualBox.(none)>
2013-09-24 20:35:28 -0400

    created best function for haoxin

--- 7/luo_haoxin/try2err.py
+++ 7/luo_haoxin/try2err.py
@@ -10,5 +10,9 @@ def main():
     
     err()
 
+    haoxin = raw_input("How many times do you want to smack haoxin?":)
+    for w in range(1,int(haoxin)+1):
+        print("Haoxin got smacked!"
+
 if __name__ == "__main__":
     main()
