531f24d074712166d6c3997cb3d1a22e8106832e
Yaseen <yislam96@gmail.com>
2013-09-25 00:07:38 -0400

    im hoping this does something by adding it first

--- 7/islam_yaseen/swagtastic.py
+++ 7/islam_yaseen/swagtastic.py
@@ -1,1 +1,11 @@
-swagtastic
+import random
+#not swagtastic, but swagunderful
+
+def swagsauce():
+    if random.random()*10 > 5:
+        return "snapbacks"
+    else:
+        return "#swag"
+
+#testing
+print swagsauce()
