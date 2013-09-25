4b980b92f375c543a55218b4f3c128ac38c06877
JUSTIN <justin@ubuntu.(none)>
2013-09-24 21:32:37 -0400

    testing for errors

--- 7/Khan_Sabrina/Fact.py
+++ 7/Khan_Sabrina/Fact.py
@@ -10,3 +10,22 @@ def multiples(n):
 print multiples(10)
 print multiples(23)
 
+def fib(n):
+    if(n < 1):
+        return -1
+    a = 1
+    b = 1
+    c = 2
+    if(n == 1 or n == 2):
+        return 1
+    n = n - 3
+    while(n > 0):
+        a = b
+        b = c
+        c = a + b
+        n = n - 1
+    return c
+
+
+for i in range(1, 20):
+    print str(fib(i)) + " "
