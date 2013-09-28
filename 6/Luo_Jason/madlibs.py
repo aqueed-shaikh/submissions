787ee63a2ad5d5e791490a7b82f4c68934b87637
Marlena <marlena.lui@gmail.com>
2013-09-27 22:51:56 -0400

    editing madlibs.py

--- 6/Luo_Jason/madlibs.py
+++ 6/Luo_Jason/madlibs.py
@@ -1,3 +1,64 @@
 #!/usr/bin
 
-print "this is it"
+
+template = "One day %(PROPERNOUN)s was %(VERB)s through the %(PLACE)s. There he found a(n) %(ADJECTIVE)s %(NOUN)s."
+
+propernouns = {1 = "Lilo",
+               2 = "Stitch",
+               3 = "Nemo",
+               4 = "Elmo",
+               5 = "Shrek",
+               6 = "Spongebob",
+               7 = "Bill",
+               8 = "Plankton",
+               9 = "Sandy",
+               10 = "Squishy"}
+
+verbs = {1 = "galloping",
+         2 = "paddling",
+         3 = "discouraging",
+         4 = "spinning",
+         5 = "running",
+         6 = "stabbing",
+         7 = "tickling",
+         8 = "shrieking",
+         9 = "folding",
+         10 = "eating"}
+
+places = { 1 = "BMCC",
+           2 = "sushi buffet",
+           3 = "Duane Reade",
+           4 = "roof",
+           5 = "cupboard under the stairs",
+           6 = "room 305"
+           7 = "Hudson staircase",
+           8 = "Starbucks",
+           9 = "Chipotle",
+           10 = "12 Grimmauld Place"}
+
+adjectives = { 1 = "ugly",
+               2 = "really ugly",
+               3 = "sparkling",
+               4 = "querulous",
+               5 = "facetious",
+               6 = "fulminating",
+               7 = "phlegmatic",
+               8 = "laconic",
+               9 = "tortuous",
+               10 = "pusillaminous"}
+#started using SAT words lol
+
+nouns = { 1 = "charlatan",
+          2 = "mountebank",
+          3 = "sashimi",
+          4 = "pokeball",
+          5 = "cow",
+          6 = "Sirius Black",
+          7 = "Jabberwocky",
+          8 = "gopher",
+          9 = "lexicon",
+          10 = "entrenchment"}
+
+#started asking bill
+
+
