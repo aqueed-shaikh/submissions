051f5c3cc9670e933e1658c5bd17f66636e1f2d7
takchiwan <ray325789@yahoo.com>
2013-09-28 19:14:52 -0400

    Update app.py

--- 7/wan_takchi/madlib/app.py
+++ 7/wan_takchi/madlib/app.py
@@ -14,9 +14,13 @@ def home():
 def generate():
   s="""
     One day, %(name)s went to the %(place)s airport. Moments before boarding, %(thing)s 
-    fell from his bag. He picked it up and %(verb)s to the gate. %(time)i %(timeunit)s passed 
-    and the plane finally lifted off.
-    
+    fell from his bag. He picked it up and %(verb)s to the gate. %(num)i %(timeunit)s passed 
+    and the plane finally lifted off. When the %(adj)% flight attendant approached him asking if he
+    wanted something to drink, he reponded with, "I would like %(drink)s please." The attendant filled
+    up the cup with %(drink)s instead and was about to hand it over, but spilled it all over. At that very 
+    moment, the %(adj)s flight captain fell asleep, and so did the co-pilot, so the plane went out of control.
+    %(num)i %(timeunit)s later, the plane crashed onto an uncharted island. There were %(time)i casualties. 
+    At that time, %(num)i supposedly extinct creatures surrounded them and ate them all. And then there were none.
     """
     
     
