181b7f1362c63cea4bba2cdee02b7d9f3ff1c57d
vivi321 <vivianvivianvivianv@gmail.com>
2013-09-28 00:19:25 -0400

    Added in words and line breaks

--- 7/leung_alvin/3-madlibs/app.py
+++ 7/leung_alvin/3-madlibs/app.py
@@ -5,12 +5,12 @@ import random
 app = Flask(__name__)
 
 blanks = {
-    'NAMES': [],
-    'PLACES': [],
-    'NOUNS': [],
-    'ADJS': [],
-    'VERBS': [],
-    'ADVS': []
+    'NAMES': ['Elmo','Pikachu','Sheldon','Batman','Mr. Z'],
+	'PRONOUNS': ['he','she','they','me','you','we'],
+    'NOUNS': ['potatoes','lollipops','tornadoes','peanut butter','bacon','dollars'],
+    'ADJS': ['crazy','big','stinky','lonely','boring'],
+    'VERBS': ['sing','PMS', 'sleep'],
+    'ADVS': ['lovingly','happily','crazily','angrily']
 }
 
 @app.route("/")
@@ -19,7 +19,33 @@ def home():
 
 @app.route("/madlibs")
 def madlibs():
-    return render_template("madlibs.html",place = "forest",name = "Mr. Z")
+    return render_template("madlibs.html",
+		name1 = blanks['NAMES'][random.randrange(0,len(blanks['NAMES']))],
+		name2 = blanks['NAMES'][random.randrange(0,len(blanks['NAMES']))],
+		name3 = blanks['NAMES'][random.randrange(0,len(blanks['NAMES']))],
+		pro1 = blanks['PRONOUNS'][random.randrange(0,len(blanks['PRONOUNS']))],
+		pro2 = blanks['PRONOUNS'][random.randrange(0,len(blanks['PRONOUNS']))],
+		noun1 = blanks['NOUNS'][random.randrange(0,len(blanks['NOUNS']))],
+		noun2 = blanks['NOUNS'][random.randrange(0,len(blanks['NOUNS']))],
+		noun3 = blanks['NOUNS'][random.randrange(0,len(blanks['NOUNS']))],
+		noun4 = blanks['NOUNS'][random.randrange(0,len(blanks['NOUNS']))],
+		noun5 = blanks['NOUNS'][random.randrange(0,len(blanks['NOUNS']))],
+		noun6 = blanks['NOUNS'][random.randrange(0,len(blanks['NOUNS']))],
+		noun7 = blanks['NOUNS'][random.randrange(0,len(blanks['NOUNS']))],
+		adj1 = blanks['ADJS'][random.randrange(0,len(blanks['ADJS']))],
+		adj2 = blanks['ADJS'][random.randrange(0,len(blanks['ADJS']))],
+		adj3 = blanks['ADJS'][random.randrange(0,len(blanks['ADJS']))],
+		adj4 = blanks['ADJS'][random.randrange(0,len(blanks['ADJS']))],
+		adj5 = blanks['ADJS'][random.randrange(0,len(blanks['ADJS']))],
+		adj6 = blanks['ADJS'][random.randrange(0,len(blanks['ADJS']))],
+		adj7 = blanks['ADJS'][random.randrange(0,len(blanks['ADJS']))],
+		verb1 = blanks['VERBS'][random.randrange(0,len(blanks['VERBS']))],
+		verb2 = blanks['VERBS'][random.randrange(0,len(blanks['VERBS']))],
+		verb3 = blanks['VERBS'][random.randrange(0,len(blanks['VERBS']))],
+		adverb1 = blanks['ADVS'][random.randrange(0,len(blanks['ADVS']))],
+		adverb2 = blanks['ADVS'][random.randrange(0,len(blanks['ADVS']))],
+		adverb3 = blanks['ADVS'][random.randrange(0,len(blanks['ADVS']))],
+	)
 
 if __name__ == "__main__":
     app.debug=True
