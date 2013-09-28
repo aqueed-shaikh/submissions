ca192ba55869f0f40e1bb007250b335391438439
thejasonluo <thejasonluo@gmail.com>
2013-09-27 23:56:31 -0400

    edit 3

--- 6/Luo_Jason/madlibs.py
+++ 6/Luo_Jason/madlibs.py
@@ -1,65 +1,24 @@
-#!/usr/bin
-
+#!/usr/bin/python
+import random
 
 template = "One day %(PROPERNOUN)s was %(VERB)s through the %(PLACE)s. There he found a(n) %(ADJECTIVE)s %(NOUN)s."
 
-propernouns = {1 = "Lilo",
-               2 = "Stitch",
-               3 = "Nemo",
-               4 = "Elmo",
-               5 = "Shrek",
-               6 = "Spongebob",
-               7 = "Bill",
-               8 = "Plankton",
-               9 = "Sandy",
-               10 = "Squishy"}
+i = random.randint(1,10)
+pr =["Lilo", "Stitch", "Nemo", "Elmo", "Shrek", "Spongebob", "Bill", "Plankton", "Sandy", "Squishy"]
+vb = ["galloping", "paddling", "discouraging", "spinning", "running", "stabbing", "tickling", "shrieking", "folding", "eating"]
+pl = ["BMCC", "sushi buffet", "Duane Read", "roof", "cupboard under the stairs", "room 305", "Narnia", "pokeball", "cow", "helicopter", "Starbucks", "Chipotle", "12 GRimmauld Place", "my apartment"]
+ad = [ "ugly", "really ugly", "sparkling", "querulous", "facetious", "fulminating" , "phlegmatic", "laconic", "tortuous", "pussilanimous"]
+ns = [ "trashcan" , "building", "phone", "cactus", "sun", "folder", "car", "shirt", "fruits", "hair", "lychee"]
 
-verbs = {1 = "galloping",
-         2 = "paddling",
-         3 = "discouraging",
-         4 = "spinning",
-         5 = "running",
-         6 = "stabbing",
-         7 = "tickling",
-         8 = "shrieking",
-         9 = "folding",
-         10 = "eating"}
+d = { 'PROPERNOUN' : random.choice(pr),
+      'VERB' : random.choice(vb),
+      'PLACE' : random.choice(pl),
+      'ADJECTIVE' : random.choice(ad),
+      'NOUN' : random.choice(ns)
+}
 
-places = { 1 = "BMCC",
-           2 = "sushi buffet",
-           3 = "Duane Reade",
-           4 = "roof",
-           5 = "cupboard under the stairs",
-           6 = "room 305"
-           7 = i",
-          4 = "pokeball",
-          5 = "cow",
-          6 = "Sirius Black",
-          7 = "Jabberwocky",
-          8 = "gopher",
-          9 = "lexicon",
-          10 = "entrenchment"}"Hudson staircase",
-           8 = "Starbucks",
-           9 = "Chipotle",
-           10 = "12 Grimmauld Place"}
 
-adjectives = { 1 = "ugly",
-               2 = "really ugly",
-               3 = "sparkling",
-               4 = "querulous",
-               5 = "facetious",
-               6 = "fulminating",
-               7 = "phlegmatic",
-               8 = "laconic",
-               9 = "tortuous",
-               10 = "pusillanimous"}
-#started using SAT words lol
-
-nouns = { 1 = "charlatan",
-          2 = "mountebank",
-          3 = "sashim
-
-#started asking bill
+print template %(d)
 
 
 
