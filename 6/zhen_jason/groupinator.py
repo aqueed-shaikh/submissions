62e1c1f92c0c10b808635e6e229ef3a0ccf34a62
Jason Zhen <jzhen46@gmail.com>
2013-09-29 17:48:28 -0400

    Original Comment

--- 6/zhen_jason/groupinator.py
+++ 6/zhen_jason/groupinator.py
@@ -3,10 +3,12 @@ from random import shuffle
 lines =open('students').readlines()
 p6=[]
 p7=[]
+groups=[]
 
 for l in lines:
     newlines = l.strip()
     nl=newlines.split(',')
+    groups.append(newlines)
     if nl[1]=='6':
         p6.append(newlines)
     elif nl[1]=='7':
@@ -14,19 +16,12 @@ for l in lines:
 
 shuffle(p6)
 shuffle(p7)
+g=p6+p7
 
-for i in range(0,len(p6),4):
+for i in range(0,len(g),4):
     for x in range(0,4):
-        p6[i+x]=p6[i+x].split(',')
-        p6[i+x][0]=str(i/4)
-        p6[i+x].pop(1)
-        p6[i+x]=",".join(p6[i+x])
-        print p6[i+x]
-
-for i in range(0,len(p7),4):
-    for x in range(0,4):
-        p7[i+x]=p7[i+x].split(',')
-        p7[i+x][0]=str(i/4)
-        p7[i+x].pop(1)
-        p7[i+x]=",".join(p7[i+x])
-        print p7[i+x]
+        g[i+x]=g[i+x].split(',')
+        g[i+x][0]=str(i/4+1)
+        g[i+x].pop(1)
+        g[i+x]=",".join(g[i+x])
+        print g[i+x]
