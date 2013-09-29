4d452b7ef19240e013c462d98e98b1d293f72e13
Ben Kurtovic <ben.kurtovic@verizon.net>
2013-09-29 18:24:19 -0400

    Sync word list with Jing

--- 6/kurtovic_benjamin/3-madlibs/app.py
+++ 6/kurtovic_benjamin/3-madlibs/app.py
@@ -9,9 +9,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("madlibs.html", d = {
-            "P1": "Brian", "P2": random.choice(["Seven", "Six", "Four", "Roger"])
-        })
+    words = {
+        "P1": random.choice(["Brian", "Ben", "Jing"]),
+        "P2": random.choice(["Seven", "Six", "Four", "Roger"]),
+        "V1": random.choice(["Kick", "Eat", "Step on", "Laugh at", "Sneeze on", "Chase"]),
+        "N1": random.choice(["Trees", "Redbull", "Snicker Bars", "Chikcen Wings", "Mac Keyboards"]),
+        "V2": random.choice(["Pray to", "Rub", "Wash", "Touch", "Sing at"]),
+        "N2": random.choice(["Cloud", "Mountain", "Chair", "Bacon"]),
+        "N3": random.choice(["Cheddar Cheese", "King", "Waffle", "Peanut Butter", "Jelly"]),
+        "N4": random.choice(["People", "Flowers", "Grass", "Water", "Mitsubishi"]),
+        "N5": random.choice(["Sharnadoes", "Whole Wheat Bread", "Apples", "Frying Pans"]),
+        "V3": random.choice(["Steal", "Devour", "Jumping On", "Crying On", "Licking"]),
+        "N6": random.choice(["Lollipops", "Honey", "Bears", "Frogs", "Pigeons"])
+    }
+    return render_template("index.html", d=words)
 
 if __name__ == "__main__":
     app.run(debug=True)
