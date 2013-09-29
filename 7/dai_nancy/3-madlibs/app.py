c5fa9834e3b58dd1651484e29ce368f65cc6420a
xxkurotenshixx <babycarr_6193@hotmail.com>
2013-09-28 22:39:23 -0400

    Update

--- 7/dai_nancy/3-madlibs/app.py
+++ 7/dai_nancy/3-madlibs/app.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask import render_template
 
 import random
-import groupinator
 
 app = Flask(__name__)
 
@@ -81,15 +80,6 @@ def madlibs():
         adverb2 = words["Adverbs"][random.randrange(0, len(words["Adverbs"]))],
         pastverb = words["PastVerbs"][random.randrange(0, len(words["PastVerbs"]))]
     )
-
-@app.route("/groupinate")
-def groupinate():
-    temp = groupinator.groupinator()
-    name = ""
-    li = "%i, %s, %s \n"
-    for l in temp:
-        name = name + li%(l[0], l[1], l[2])
-    return render_template("groupinator.html", name)
 
 if __name__ == "__main__":
     app.debug = True
