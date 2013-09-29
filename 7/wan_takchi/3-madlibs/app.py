e0ed0a1fb3c87727ab56903b4a525502ac47b964
Tak Chi <tak@tak-VirtualBox.(none)>
2013-09-28 20:46:07 -0400

    finish

--- 7/wan_takchi/3-madlibs/app.py
+++ 7/wan_takchi/3-madlibs/app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from flask import Flask
 from flask import render_template
 
@@ -18,17 +19,17 @@ def generate():
   timeunits= ["years", "seconds", "minutes", "hours", "weeks", "millenia"]
   adjs=["fat", "monstrous", "hot", "ugly", "dirty", "beautiful", "cute"]
   drinks=["apple juice", "orange juice", "saliva", "mud", "dirt", "urine", "poop extract"]
-  dictionary = {"name" : random.choice(name),
-                "place" : random.choice(places),
-                "thing" : random.choice(things),
-                "time": random.choice(times),
-                "timeunit": random.choice(timeunits),
-                "adj": random.choice(adjs),
-                "drink": random.choice(drinks)
-                }
-  return render_template("story.html")
+  return render_template("story.html",
+                         name = random.choice(names),
+                         place = random.choice(places),
+                         thing = random.choice(things),
+                time=random.choice(times),
+                timeunit= random.choice(timeunits),
+                adj= random.choice(adjs),
+                drink= random.choice(drinks))
+                         
 
 if __name__ == '__main__':
   app.debug=True
-  app.run()
-  
+  app.run(host="0.0.0.0", port=5008)
+ 
