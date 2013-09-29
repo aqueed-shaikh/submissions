bff75c3ee520324669b6765bf41d3e0c1154eea2
Kelvin <kelvin@ubuntu.(none)>
2013-09-28 22:12:55 -0400

    Madlibs solo ):

---
+++ 7/mei_kelvin/3-madlibs/madlibs.py
@@ -0,0 +1,9 @@
+template = """
+<h1>Cards against Humanity 1</h1>
+<p>I'm sorry Professor, but I couldn't complete my homework because of %(answer1)s. </p>
+<h1>Cards against Humanity 2</h1>
+<p>Instead of coal, Santa now gives the bad children %(answer2)s. </p>
+<h1>Cards against Humanity 3</h1>
+<p> In Michael Jackson's final moments, he thought about %(answer3)s. </p>
+"""
+
