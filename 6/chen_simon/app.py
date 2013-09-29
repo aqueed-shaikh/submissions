cac47519ed27d83e5983bcd91d0d70908b0f90ef
Simon <simon@ubuntu.(none)>
2013-09-29 14:23:37 -0400

    MadLibs Story added.

--- 6/chen_simon/app.py
+++ 6/chen_simon/app.py
@@ -28,7 +28,41 @@ def madLibs():
     noun = ['noob', 'elite', 'poop', 'diarrhea', 'rock', 'cat', 'dog', 'mouse' , 'dragon', 'book', 'SAT', 'Poi', 'pillow', 'sun', 'table', 'bed', 'water', 'fire']
     random.shuffle(n)
     random.shuffle(adj)
-    d = {'name' : n.pop()}
+    random.shuffle(place);
+    random.shuffle(noun);
+    d = {'name' : n.pop(),
+         'name1' : n.pop(),
+         'name2' : n.pop(),
+         'name3' : n.pop(),
+         'name4' : n.pop(),
+         'name5' : n.pop(),
+         'name6' : n.pop(),
+         'name7' : n.pop(),
+         'adj' : adj.pop(),
+         'adj1' : adj.pop(),
+         'adj2' : adj.pop(),
+         'adj3' : adj.pop(),
+         'adj4' : adj.pop(),
+         'adj5' : adj.pop(),
+         'adj6' : adj.pop(),
+         'adj7' : adj.pop(),
+         'place' : place.pop(),
+         'place1' : place.pop(),
+         'place2' : place.pop(),
+         'place3' : place.pop(),
+         'place4' : place.pop(),
+         'place5' : place.pop(),
+         'place6' : place.pop(),
+         'place7' : place.pop(),
+         'noun' : noun.pop(),
+         'noun1' : noun.pop(),
+         'noun2' : noun.pop(),
+         'noun3' : noun.pop(),
+         'noun4' : noun.pop(),
+         'noun5' : noun.pop(),
+         'noun6' : noun.pop(),
+         'noun7' : noun.pop(),};
+    
     return render_template("madlibs.html", d = d)
 
 if __name__ == "__main__":
