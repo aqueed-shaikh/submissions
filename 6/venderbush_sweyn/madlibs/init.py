f56ebaaff795d2ce7c6f2d83cc2cb30c32f9a04f
Aaron Coppa <aacoppa@gmail.com>
2013-09-29 12:38:17 -0400

    Finished the madlibs bible

---
+++ 6/venderbush_sweyn/madlibs/init.py
@@ -0,0 +1,3 @@
+from madlibs import initDict
+
+initDict()
--- 6/venderbush_sweyn/madlibs/madlibs.py
+++ 6/venderbush_sweyn/madlibs/madlibs.py
@@ -4,24 +4,46 @@ import shelve
 from random import choice
 app = Flask(__name__)
 
-wordsDict = {
-            "name":["bob", "joe", "aaron", "sweyn"],
-            "noun":["tree", "cow"]
-            }
-
-
+words={}
+nouns=["ninja", "chair", "pancake", "statue", "unicorn", "rainbows", "laser beams", "senor", "bunny", "captain", "nibblets", "cupcake", "carrot", "gnome", "glitter", "potato", "salad", "toejam", "curtain"]
+adjectives=["shining", "crispy", "soaring", "endless", "sparkling", "fluttering", "spiky", "scrumptious", "eternal", "slimy", "slick", "gilded", "ancient", "smelly", "glowing", "rotten", "decrepit", "lousy", "grimy", "rusty", "sloppy", "muffled", "foul", "rancid", "fetid"]
+names = ["jim", "felix", "rochanelle", "liquanaishanda", "sweynaynagram", "sean toodle" ]
+verbs = ["paint", "dance", "swim", "twerk"]
+places = ["Tihuana", "Aruba", "Jupiter", "South Austrailia" ]
+words['verb'] = verbs
+words['noun'] = nouns
+words['adjective'] = adjectives
+words['name'] = names
+words['place'] = places
+def initDict():
+	Genesis = shelve.open("values/genesis")
+	Genesis['noun'] = 3
+	Genesis['name'] = 1
+	Genesis['adjective'] = 2
+	Genesis['verb'] = 3
+	Genesis['place'] = 1
+	Genesis.close()
+	exodus = shelve.open("values/exodus")
+	exodus['noun'] = 0
+	exodus['name'] = 2
+	exodus['adjective'] = 2
+	exodus['verb'] = 2
+	exodus['place'] = 1
+	exodus.close()
 def createDict(d):
     passDict = {}
     for key in d:
-        for i in range(d[key]):
-            passDict[key+str(i)] = choice(wordsDict[key])
+	for i in range(d[key]):
+		passDict[key+str(i)] = choice(words[key])
+			
     return passDict
 
 @app.route("/")
 @app.route("/<name>")
-def root(name="default"):
+def root(name="genesis"):
     values = shelve.open("values/" + name)
     d=createDict(values)
+    print(d)
     return render_template(name + ".html", d=d)
 
 @app.route('/favicon.ico')
