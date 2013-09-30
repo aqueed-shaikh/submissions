400488c799c3372ce2d72807471d6213044e13d0
RatedRion <ratedrion@gmail.com>
2013-09-29 20:17:07 -0400

    Added fun stuff

--- 6/wu_jia sen/3-madlibs/app.py
+++ 6/wu_jia sen/3-madlibs/app.py
@@ -12,19 +12,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def lottery():
-    ball1=random.randrange(1,57)
-    ball2=random.randrange(1,57)
-    ball3=random.randrange(1,57)
-    ball4=random.randrange(1,57)
-    ball5=random.randrange(1,57)
-    megaball=random.randrange(1,47)
+    ball1 = random.randrange(1,57)
+    ball2 = random.randrange(1,57)
+    ball3 = random.randrange(1,57)
+    ball4 = random.randrange(1,57)
+    ball5 = random.randrange(1,57)
+    megaball = random.randrange(1,47)
     return render_template("index.html",name=request.remote_addr,num1=ball1,num2=ball2,num3=ball3,num4=ball4,num5=ball5,num6=megaball)
 
 @app.route("/madlibs")
 def madlibs():
-    pnoun = ["Claus", "Brown", "Zamansky", "Platek", "Dursley", "Jaishankar", "Wu", "Yuen"]
-	pnouns = pnoun[randrange(0, len(pnoun))]
-	return render_template("madlibs.html", propernoun = pnouns)
+    pnoun = [ "Dursley", "Claus", "Brown", "Zamansky", "Platek", "Jaishankar", "Wu", "Yuen"]
+	adj = ["normal", "crazy", "stupid", "annoying", "caring", "benevolent", "lazy", "awkward", "awful"]
+	pnouns = pnoun[random.randrange(0, len(pnoun))]
+	adjs = adj[random.randrange(0, len(adj))]
+	return render_template("madlibs.html", propernoun=pnouns, adjective=adjs)
 
 	
 if __name__=="__main__":
