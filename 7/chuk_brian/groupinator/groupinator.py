ffc905e5094f1c50833c250430fa83cc580e0e61
Yaseen <yislam96@gmail.com>
2013-09-30 13:09:02 -0400

    hey chuk, i added shtuff

--- 7/chuk_brian/groupinator/groupinator.py
+++ 7/chuk_brian/groupinator/groupinator.py
@@ -1,4 +1,7 @@
-lines = open(students).readlines()
+lines = open("students").readlines()
 newlines = []
-for l in lines: newlines = l.strip()
+for l in lines: newlines.append(l.strip())
 print newlines
+newerlines=[]
+for i in newlines:newerlines.append(i.split(","))
+print newerlines
