32ecddacb5fd5c18a16180e9cce09b0abbf74875
Roger Li <rog3rli@gmail.com>
2013-09-30 23:09:19 -0400

    SIX LINES

--- 6/li_roger/groupinator.py
+++ 6/li_roger/groupinator.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python
 import random, math
 s = [[],[]]
-for l in open("students").readlines():
-	l = l.strip().split(",")[1:]
-	s[int(l[0])-6].append(l[1:])
+[s[int(l.split(",")[0])-1].append(l.strip().split(",")[2:]) for l in open("students").readlines()]
 [random.shuffle(i) for i in s]
-s = s[0] + s[1]
-[print(str(math.floor(i/4)+1)+","+(",".join(s[i]))) for i in range(len(s))]
+[print(str(math.floor(i/4)+1)+","+(",".join((s[0]+s[1])[i]))) for i in range(len(s[0]+s[1]))]
