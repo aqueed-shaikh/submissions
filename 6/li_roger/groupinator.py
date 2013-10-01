181b94fa680af484d4ecaef92532ffa80cebaf98
Roger Li <rog3rli@gmail.com>
2013-09-30 23:16:11 -0400

    compatibility with python 2.7

--- 6/li_roger/groupinator.py
+++ 6/li_roger/groupinator.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 import random, math
 s = [[],[]]
-[s[int(l.split(",")[0])-1].append(l.strip().split(",")[2:]) for l in open("students").readlines()]
+for l in open("students").readlines(): s[int(l.split(",")[0])-1].append(l.strip().split(",")[2:])
 [random.shuffle(i) for i in s]
-[print(str(math.floor(i/4)+1)+","+(",".join((s[0]+s[1])[i]))) for i in range(len(s[0]+s[1]))]
+for i in range(len(s[0]+s[1])): print(str(math.floor(i/4)+1)+","+(",".join((s[0]+s[1])[i])))
