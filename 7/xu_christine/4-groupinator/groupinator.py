05910d49a350ef384960a9548fa5feb333df38b3
JUSTIN <justin@ubuntu.(none)>
2013-10-01 19:01:22 -0400

    copied files into partner's folder

--- 7/xu_christine/4-groupinator/groupinator.py
+++ 7/xu_christine/4-groupinator/groupinator.py
@@ -1,31 +1,59 @@
+#Groupinator completed by Justin Duda and Christine Xu
+
 import random
 
-lines = open ("groupinatorstudents", 'foo')
-foo = lines.read()
+lines = open("students.txt").readlines();
 
-lines = foo.read()
- lines.remove("")
- lines.split(",");
+newlines = []
 
-def groups():
+for l in lines: 
+  newlines.append( l.strip() )
+
+length = len(newlines)
+
+#print newlines
+for i in range(0, length):
+ # print l
+  newlines[i] = newlines[i].split(",")
+  #print l
+  newlines[i].pop(0)
+  newlines[i].pop(0)
 
 pd6 = []
 pd7 = []
+groups = []
 
 
-for (i=0; i <32; i++):
-  pd6.append(lines[i]);
 
-for (i=32; i< 65; i++):
-  pd7.append (lines[i]);
+for i in range (0, length/2):
+  pd7.append(newlines.pop())
 
-random.shuffle(pd6);
-random.shuffly(pd7);
+#print pd7
 
+for i in range (0, length - length/2):
+  pd6.append(newlines.pop())
 
-def print():
- for i in lines:
-    
+#print "\n"
+#print pd6
 
+for i in range (0, 8):
+  for g in range (0, 4):
+    tmp = [str(i+1)] 
+    tmp.extend(pd6.pop(random.randint(0,len(pd6)-1)))
+    tmp = ",".join(tmp)
+    groups.insert(len(groups)/2, tmp)
+    tmp = [str(i+9)]
+    tmp.extend(pd7.pop(random.randint(0,len(pd7)-1)))
+    tmp = ",".join(tmp)
+    groups.append(tmp)
 
-groups()
+#print groups
+
+#for i in range(0, len(groups)):
+  #l.append("\n")
+ # newStr = ",".join(groups[i])
+  #groups[i] = newStr
+
+groups = "\n".join(groups)
+
+print groups
