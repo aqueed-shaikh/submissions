1568bbf27e25b042501fd4f5d9fb7f843f0ae2a3
Steve Zhu <st.zhu1@gmail.com>
2013-09-30 22:53:52 -0400

    Groupinator update

--- 6/zhu_steve/groupinator.py
+++ 6/zhu_steve/groupinator.py
@@ -1,26 +1,18 @@
 #!usr/bin/python
+import sys, random
 
-import sys
-import random
+if len(sys.argv) == 1: sys.argv.append("students")
 
-from flask import Flask
+l = [[],[]]
+for p in open(sys.argv[1]).readlines():
+	p = p.strip().split(',')
+	l[int(p[1])-6].append(','.join(p[2:]))
 
-people = [p.strip().split(',') for p in open(sys.argv[1]).readlines()]
+for p in l:
+	random.shuffle(p)
 
-periods = [[], []]
-for person in people:
-	index = int(person[0]) - 1
-	periods[index].append(','.join([person[2], person[3]]))
+l = l[0] + l[1] # people
+l = [l[x:x+4] for x in xrange(0, len(l), 4)] # groups
+l = [",".join([str(x + 1), group]) for x in xrange(len(l)) for group in l[x]] # people
 
-for period in periods:
-	random.shuffle(period)
-
-
-people = periods[0] + periods[1]
-groups = [people[x:x+4] for x in xrange(0, len(people), 4)]
-people = [",".join([str(x + 1), b]) for x in xrange(len(groups)) for b in groups[x]]
-
-print people
-
-
-app = Flask(__name__)
+print '\n'.join(l)
