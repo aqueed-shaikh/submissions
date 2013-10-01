a9b5ac7972bd9cfa3ff189820365c7b5165e03ff
helenlin <helen51796@gmail.com>
2013-10-01 00:28:45 -0400

    deleting unnecessary files

--- 6/lin_helen/miniflask.py
+++
@@ -1,3 +0,0 @@
-print "Haha! Joke's on you because this file doesn't contain anything related to flask at all! \n \n \n That's what you get for being so gullible! \n \n \n"
-
-print "YOU GET TRICKED."
