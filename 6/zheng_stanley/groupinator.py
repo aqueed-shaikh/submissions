286b3f8085df2172a5ce24923596a7843a3eac43
StanleyZheng <szheng1689@gmail.com>
2013-10-01 23:04:56 -0400

    Groupinator HW

--- 6/zheng_stanley/groupinator.py
+++ 6/zheng_stanley/groupinator.py
@@ -18,10 +18,11 @@ for name in striplines:
         period7.append(name)
 
 
-#for period 6
+#prints groups for period 6
+#get rid of excess info
 for peers in period6:
     peers.lstrip("1,6,")
-
+#shuffle period6-(list)
 random.shuffle(period6)
 counter = 1
 group = 1
@@ -29,13 +30,16 @@ for peers in period6:
     if counter > 4:
         group = group + 1
         counter = 1
-    print group, peers[3:]
+        print ''
+    print group, peers[4:]
     counter = counter + 1
 
+
 #prints groups for period 7
+#get rid of excess info
 for others in period7:
     others.lstrip("2,7,")
-
+#shuffle period7-(list)
 random.shuffle(period7)
 counter2 = 1
 group2 = 9
@@ -43,6 +47,7 @@ for others in period7:
     if counter2 > 4:
         group2 = group2 + 1
         counter2 = 1
-    print group2, others[3:]
+        print ''
+    print group2, others[4:]
     counter2 = counter2 + 1
 
