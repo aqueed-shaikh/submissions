1f3d393d7d6608e0b5675c4b8e585c6875090c59
Yaseen <yislam96@gmail.com>
2013-10-01 21:06:00 -0400

    groupinator: major change to layout of html lists

--- 7/islam_yaseen/groupinator/groupinator.py
+++ 7/islam_yaseen/groupinator/groupinator.py
@@ -19,24 +19,28 @@ def groups():
     listofpd6names=[]
     for i in newerlines:
         if i[1]=='6':
-            listofpd6names.append(i[2]+','+i[3])
+            listofpd6names.append(i[2]+', '+i[3])
 
 
     listofpd7names=[]
     for j in newerlines:
         if j[1]=='7':
-            listofpd7names.append(j[2]+','+j[3])
+            listofpd7names.append(j[2]+', '+j[3])
     
     results6=""
     localpd6=listofpd6names
     for i in range(1,9):
+        results6+='<li>Group '+str(i)+':<ul>'
         for j in range(0,4):
-            results6+='<li>'+str(i)+','+localpd6.pop(int(random.random()*len(localpd6)))+'</li>'
+            results6+='<li>'+localpd6.pop(int(random.random()*len(localpd6)))+'</li>'
+        results6+='</ul></li>'
     results7=""
     localpd7=listofpd7names
     for i in range(9,17):
+        results7+='<li>Group '+str(i)+':<ul>'
         for j in range(0,4):
-            results7+='<li>'+str(i)+','+localpd7.pop(int(random.random()*len(localpd7)))+'</li>'
+            results7+='<li>'+localpd7.pop(int(random.random()*len(localpd7)))+'</li>'
+        results7+='</ul></li>'
     return """
 <h1>Created by Brian Chuk and Yaseen Islam</h1>
 <h2>Period 6</h2>
