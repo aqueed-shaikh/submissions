ecc2ee09cd500fd6cabbb95827a783edb68a567f
Severyn Kozak <severyn.kozak@gmail.com>
2013-10-06 12:33:08 -0400

    Update forms to "post"; add shelve.
    Update form methods to "post" (initially, data wasn't being sent from
    the client, and app.py interpreted submissions as just GET requests).
    Add basic dictionary functionality to app.py.

--- login/6/kozak_severyn_yuen_jared/app.py
+++ login/6/kozak_severyn_yuen_jared/app.py
@@ -1,10 +1,10 @@
 """
 app.py is a login engine.
-
 """
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
+import shelve
 
 app = Flask(__name__)
 
@@ -19,14 +19,22 @@ def index():
 def login():
 	if request.method == "GET":
 		return render_template("login.html")
+	else:
+		Username = request.form["Username"].encode("ascii", "ignore")
+		Password = request.form["Password"].encode("ascii", "ignore")
 
+		#still debugging
+		data = shelve.open("appData")
+		data[Username] = Password
+		data.close()
+		return "Success."
 
 @app.route("/register", methods = ["GET", "POST"])
 
 def register():
 	if request.method == "GET":
 		return render_template("register.html")
-	else:
+	#else:
 
 
 #@app.route("/vault")
