67ff37c3c54582e9725a92e687af0b9d93844e8e
thejasonluo <thejasonluo@gmail.com>
2013-10-05 23:48:11 -0400

    im so tired

--- 6/lui_marlena/login.py
+++ 6/lui_marlena/login.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from flask import session, url_for, request, redirect, render_template
+from flask.ext import shelve
 
 app = Flask(__name__)
+app.config['SHELVE_FILENAME'] = 'username.db'
+shelve.init_app(app)
 
+@app.route("/register")
+def register():
+    page="""<h1>Register</h1>
+        <form method="post">
+        <input type="text" name="username">
+        <input type="text" name="password">
+        <input type="submit" name="button" value="register">
+        <input type="submit" name="button" value="cancel">
+        </form>
+        """
+    username = shelve.get_shelve('c')
+    username["username"] = "password"
+    return page
 
-
-
-
-
-
-
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.debug = True
     app.run(host = "0.0.0.0", port=5000)
