9ce32214b5aa30f7b782fa271f07d5a1d7d60b0b
Jane Argodale <jja8@Jane-Argodales-MacBook-Pro.local>
2013-10-06 16:32:31 -0400

    currently working on register

---
+++ login/6/rein_dorit_argodale_jane/app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from flask import request, render_template, redirect, url_for, session
+from flask.ext import shelve
+
+app=Flask(__name__)
+app.secret_key="key"
+app.config['SHELVE_FILENAME'] = 'logins'
+shelve.init_app(app)
+
+@app.route("/home")
+def home():
+    if "username" in session:
+        return "<h1>Hello!</h1>"
+    else:
+	return redirect(url_for('login'))
+
+@app.route("/register")
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        username = request.form["username"].encode("ascii","ignore")
+        password = request.form["password"].encode("ascii","ignore")
+        users = shelve.get_shelve()
+        if users.has_key(username):
+            return render_template("register.html")
+        users[username] = password
+        session["username"] = username
+        return redirect(url_for("home"))
+
+@app.route("/login") #placeholder for login page
+def login():
+    return "<h1>lalala</h1>"
+
+if __name__=="__main__":
+    app.debug = True
+    app.run()
