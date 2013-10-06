84e2e077bacedf7c8ede56290b59c4e11a401808
Will <flyinbananatree@me.com>
2013-10-06 16:36:22 -0400

    added login page

--- login/7/field-thompson_will_luo_haoxin/login.py
+++ login/7/field-thompson_will_luo_haoxin/login.py
@@ -1,3 +1,31 @@
 # Will Field-Thompson & Haoxin Luo
 
 from flask import Flask
+import flask.ext.shelve
+from flask.ext.shelve import init_app
+from flask import render_template
+
+app = flask.Flask(__name__)
+# Configuring shelve:
+app.config['SHELVE_FILENAME'] = 'users.dat'
+app.config['SHELVE_WRITEBACK'] = True
+
+init_app(app)
+
+@app.route('/')
+def home():
+    return "<h1>home</h1>"
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
