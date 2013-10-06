dda297ae72ded2b624ed794f0d14939f9e5bf01c
Benjamin Attal <attal.benjamin@gmail.com>
2013-10-06 16:00:56 -0400

    Modifications to app.py file, added templates

--- login/7/attal_benjamin_lin_michele/app.py
+++ login/7/attal_benjamin_lin_michele/app.py
@@ -4,56 +4,55 @@ from flask.ext import shelve
 app = Flask(__name__)
 app.config['SHELVE_FILENAME'] = 'shelve.db'
 shelve.init_app(app)
-app.secret_key='my secret key'
+app.secret_key='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-@app.route("/")
+@app.route('/')
 def home():
     if 'username' in session:
         return render_template('home.html')
     else:
         return redirect(url_for('login'))
 
-@app.route("/login", methods = ['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     if 'username' in session:
         return redirect(url_for('home'))
-    elif request.method == "GET":
-        return render_template("login.html")
+    elif request.method == 'GET':
+        return render_template('login.html')
     else:
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form['username'].encode('ascii', 'ignore')
+        password = request.form['password'].encode('ascii', 'ignore')
         database = shelve.get_shelve()
         if username not in database:
-            return redirect(url_for("register"))
+            return redirect(url_for('register'))
         elif database[username] == password:
             session['username'] = username
             return redirect(url_for('home'))
         else:
-            return render_template("login.html", message = "Please check your username and password again")
+            return render_template('login.html', message='Please check your username and password again')
 
-@app.route("/register", methods = ['GET','POST'])
+@app.route('/register', methods = ['GET','POST'])
 def register():
     if 'username' in session:
         return redirect(url_for('home'))
-    elif request.method == "GET":
-        return render_template("register.html");
+    elif request.method == 'GET':
+        return render_template('register.html');
     else:
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form['username'].encode('ascii', 'ignore')
+        password = request.form['password'].encode('ascii', 'ignore')
         database = shelve.get_shelve()
         if username in database:
-            return render_template("register.html", message = "Already in use")
+            return render_template('register.html', message='Already in use')
         else:
             database[username] = password
             session['username'] = username
             return redirect(url_for('home'))
 
         
-@app.route("/logout")
+@app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
 
-if __name__ == "__main__":
-    app.debug=True
-    app.run(host='0.0.0.0', port = 5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
