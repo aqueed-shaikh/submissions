9b12324e4dff826daebfa70cff4fd7930893d50d
Ben Kurtovic <ben.kurtovic@verizon.net>
2013-10-05 22:27:03 -0400

    Cleanup; remove some redundant code and pages.

--- login/6/kurtovic_benjamin_ye_tyrone/app.py
+++ login/6/kurtovic_benjamin_ye_tyrone/app.py
@@ -1,59 +1,37 @@
+# Benjamin Kurtovic and Tyrone Ye
+
 from flask import Flask
-#from flask import session,redirect,render_template
-from flask import session,url_for,request,redirect,render_template
+from flask import Flask, session, url_for, request, redirect, render_template
 from flask.ext import shelve
 
-
-
 app = Flask(__name__)
-app.config['SHELVE_FILENAME'] = 'login.db'
+app.config["SHELVE_FILENAME"] = "login.db"
 shelve.init_app(app)
-appt.secret_key="nLOGN"
+app.secret_key = "nLOGN"
 
 @app.route("/")
-def start():
-    if 'username' in session:
-        return redirect("/page1")
-    else :
-        return """
-<h1> Welcome <h1>
-<a href = "/login">Login</a>
-<a href = "/home">Home</a>
-"""
-
-@app.route("/login", methods=['GET','POST'])
-def login():
-    if 'username' in session:
-        return redirect("/home")
-    else:
-        if request.method=="GET":
-            return render_template("login.html");
-        else:
-            usrname = request.form['username']
-            pw = request.form['password']
-            shelve[usrname] = pw
-            
-
-@app.route("/home")
-def home():
-    if 'username' in session:
-        return render_template("home.html")
-    else:
-        return redirect("/login")
-
-@app.route("/index")
 def index():
-    if 'username' in session:
+    if "username" in session:
         return render_template("index.html")
     else:
         return redirect("/login")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if "username" in session:
+        return redirect("/")
+    elif request.method == "GET":
+        return render_template("login.html");
+    else:
+        usrname = request.form["username"]
+        pw = request.form["password"]
+        shelve[usrname] = pw
+
 @app.route("/logout")
 def logout():
-    if 'username' in session:
-        session.pop('username', None)
+    if "username" in session:
+        session.pop("username", None)
     return redirect("/")
-    
+
 if __name__=="__main__":
-    app.debug=True
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
