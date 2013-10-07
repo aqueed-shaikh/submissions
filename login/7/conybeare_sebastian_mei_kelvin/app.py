25f262a2279b93ec5799efde67a379818f652835
Kelvin <kelvin@ubuntu.(none)>
2013-10-07 17:29:08 -0400

    meh

--- login/7/conybeare_sebastian_mei_kelvin/app.py
+++ login/7/conybeare_sebastian_mei_kelvin/app.py
@@ -4,22 +4,56 @@ from flask import render_template
 from flask import request, session
 from flask import redirect, url_for
 from flask.ext import shelve
-import login
+
 app = Flask(__name__)
+app.secret_key = "cookies"
+app.config["SHELVE_FILENAME"] = 'shelves.db'
+shelve.init_app(app)
 
 @app.route("/")
 def home():
     return render_template ("index.html")
-@app.route("/login")
+@app.route("/login", methods = ['GET', 'POST'])
 def login():
-    return render_template ("login.html")
-@app.route("/register")
+    lists = shelve.get_shelve('c')
+    if request.method == "GET":
+        return render_template ("login.html")
+    else:
+        button = request.form['button']
+        if button  == 'Enter':
+            usern = request.form['Username']
+            passw = request.form['Password']
+            if usern in lists and lists[usern] == passw:
+                session['Username'] = username
+                return redirect ("/success")
+            else: 
+                return redirect ("/login")
+
+
+@app.route("/register", methods = ["GET","POST"])
 def register():
-    return render_template ("register.html")
+    lists = shelve.get_shelve('c')
+    if request.method == "GET":
+        return render_template ("register.html")
+    else:
+        if request.method == "POST":
+            usern = request.form["Username"]
+            passw = request.form["Password"]
+            cpassw = request.form["Confirm Password"]
+            if usern in lists:
+                return redirect ("/error")
+            elif passw != cpassw:
+                return redirect ("/error")
+            else:
+                lists[usern] = {passw}
+                return redirect ("/login")
+@app.route("/success")
+def sucess():
+     return render_template ("LoginSuccess.html")
 
-
-
-
+@app.route("/error")
+def error():
+    return render_template ("Error.html")
 
 if __name__=="__main__":
     app.debug=True
