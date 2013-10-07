5edbd9e3e7fb95df5623a714e13f12a6cb8877bf
Victoria Chung <vickyy.chung@gmail.com>
2013-10-06 21:00:35 -0400

    Login: first attempt

---
+++ 7/chung_victoria/login/app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import request
+from flask import url_for, render_template, redirect
+
+app = Flask(__name__)
+app.secret_key = 'my secret key'
+
+
+@app.route("/")
+def home():
+    if 'username' in session:
+        return "<h1> The main page</h1>"
+    else:
+        return redirect(url_for('login'))
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    if request.method == "GET":
+        return render_template("login.html", incorrect="False")
+    else:
+        a = request.form['username']
+        username = a.encode('ascii','ignore')
+        b = request.form['password']
+        password = b.encode('ascii','ignore')
+        s = shelve.open("sessions")
+        if s.has_key(username) and s["%s"%(username)] == password:
+            session['username'] = username
+            s.close()
+            return redirect(url_for('home'))
+        else:
+            s.close()
+            return render_template("login.html", incorrect="True")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(host="0.0.0.0",port=5005)
