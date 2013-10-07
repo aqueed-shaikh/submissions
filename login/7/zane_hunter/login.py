b33c185df7285c4c1391dbb82d84bca528068a06
hunteriam <theprinceofhesh@gmail.com>
2013-10-06 20:23:49 -0400

    Closed shelves

--- login/7/zane_hunter/login.py
+++ login/7/zane_hunter/login.py
@@ -9,13 +9,14 @@ def registerUser(app, usern, passw):
 	db = shelve.get_shelve(shelf)
 	hashpass = encrypt(passw)
 	db[usern] = hashpass
-	
+	shelve.close()
 def checkUser(app,usern,passw):
 	shelve.init_app(app)
 	db = shelve.get_shelve(shelf)
 	hashpass = encrypt(passw)
-	return db[usern] == hashpass
-
+	ans = db[usern] == hashpass
+	shelve.close()
+	return ans
 def encrypt(passw):
 	encrypter = sha.new(passw)
 	encrypter.update(secret)
