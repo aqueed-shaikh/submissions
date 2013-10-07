81273dc6583cd634c8fcdf4e14b89096e67a1dad
Steve Zhu <st.zhu1@gmail.com>
2013-10-07 00:38:03 -0400

    Update app.py

--- login/6/greene_victoria_zhu_steve/app.py
+++ login/6/greene_victoria_zhu_steve/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, url_for, redirect, request, session, flash
 from flask.ext import shelve
+from flask.ext.shelve import get_shelve
 
 app = Flask(__name__)
 app.secret_key = 'WOW SUPER SECRET KEY!1!!!!!!!!!!!!!one'
@@ -15,14 +16,15 @@ def home():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if logged_in():
+		return redirect(url_for('page1'))
 	if request.method == 'POST':
 		username = get_form_value('username')
 		password = get_form_value('password')
-		db = shelve.get_shelve('c')
+		db = get_shelve('c')
+		# add session
 		if username in db and db[username] == password:
 			session['username'] = username
-	if 'username' in sesssion and session['username'] != none:
-		return redirect(url_for('page1'))
 	return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -31,7 +33,7 @@ def register():
 		username = get_form_value('username')
 		password = get_form_value('password')
 		password_confirm = get_form_value('password-confirm')
-		db = shelve.get_shelve('c')
+		db = get_shelve('c')
 		if password != password_confirm:
 			return 'The two passwords are not equal.'
 		elif username in db:
@@ -41,13 +43,18 @@ def register():
 			return render_template('page2.html')
 	return render_template('register.html')
 
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/')
+
 @app.route('/page1')
 def page1():
-	return render_template('page1.html')
+	return logged_in() ? render_template('page1.html') : redirect(url_for('login'))
 
 @app.route('/page2')
 def page2():
-	return render_template('page1.html')
+	return logged_in() ? render_template('page2.html') : redirect(url_for('login'))
 
 @app.route('/accounts')
 def accounts():
@@ -56,11 +63,9 @@ def accounts():
 	for key in db:
 		acc += key + ":" + db[key] + "\n"
 	return acc
-	
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect('/')
+
+def logged_in():
+	return 'username' in session and session['username'] != none
 
 def get_form_value(key):
 	return request.form[key].encode('ascii', 'ignore')
