269390d029a8a6642abadd79a9e8dce9917dda07
hunteriam <theprinceofhesh@gmail.com>
2013-10-06 20:08:46 -0400

    Nearly finished

--- login/7/zane_hunter/app.py
+++ login/7/zane_hunter/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask.ext import shelve
+import login
 
 app = Flask(__name__)
 
@@ -13,9 +15,16 @@ def register():
 	
 	
 	return render_template("render.html")
-@app.route('/login/')
+@app.route('/login/',methods="GET,POST")
 def login():
-	return render_template("login.html")
+	if request.method == "POST": #post,get
+		data = request.form
+		if login.checkUser(app,data["uname"],data["passw"]):
+			return render_template("logged.html")
+		else:
+			return render_template("whoops.html")
+	else:
+		return render_template("login.html")
 
 if __name__ == "__main__":
 	app.debug = True
