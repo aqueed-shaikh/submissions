448c63b201b41efea1383fb67747d66bac5e125a
Benjamin Attal <attal.benjamin@gmail.com>
2013-10-12 20:12:06 -0400

    Follow pep8 conventions

--- login/7/attal_benjamin_lin_michele/app.py
+++ login/7/attal_benjamin_lin_michele/app.py
@@ -8,48 +8,52 @@ user_table = User('login.dat')
 
 @app.route('/')
 def home():
-  if 'username' in session:
-    return render_template('home.html')
-  return redirect(url_for('login'))
+    if 'username' in session:
+        return render_template('home.html')
+    return redirect(url_for('login'))
 
 @app.route('/something')
 def something():
-  if 'username' in session:
-    return render_template('something.html')
-  return redirect(url_for('login'))
+    if 'username' in session:
+        return render_template('something.html')
+    return redirect(url_for('login'))
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-  if 'username' in session:
-    return redirect(url_for('home'))
-  elif request.method == 'GET':
-    return render_template('login.html')
-  username = request.form['username'].lower()
-  password = request.form['password'].lower()
-  if user_table.authenticate(username, password):
-    session['username'] = username
-    return redirect(url_for('home'))
-  return render_template('login.html', message='Please check your username and password again')
+    if 'username' in session:
+        return redirect(url_for('home'))
+    elif request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username'].lower()
+    password = request.form['password'].lower()
+    if user_table.authenticate(username, password):
+        session['username'] = username
+        return redirect(url_for('home'))
+    return render_template(
+        'login.html', 
+        message='Please check your username and password again')
 
 @app.route('/register', methods = ['GET','POST'])
 def register():
-  if 'username' in session:
+    if 'username' in session:
+        return redirect(url_for('home'))
+    elif request.method == 'GET':
+        if 'message' in request.args:
+            return render_template('register.html', 
+                                    message=request.args['message'])
+        return render_template('register.html')
+    username = request.form['username'].lower()
+    password = request.form['password'].lower()
+    if user_table.exists(username):
+        return render_template('register.html', 
+                                message='Username already in use')
+    user_table.insert(username, password)
     return redirect(url_for('home'))
-  elif request.method == 'GET':
-    if 'message' in request.args:
-      return render_template('register.html', message=request.args['message'])
-    return render_template('register.html')
-  username = request.form['username'].lower()
-  password = request.form['password'].lower()
-  if user_table.exists(username):
-    return render_template('register.html', message='Username already in use')
-  user_table.insert(username, password)
-  return redirect(url_for('home'))
-    
+        
 @app.route('/logout')
 def logout():
-  session.pop('username', None)
-  return redirect(url_for('home'))
-  
+    session.pop('username', None)
+    return redirect(url_for('home'))
+    
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- login/7/attal_benjamin_lin_michele/auth.py
+++ login/7/attal_benjamin_lin_michele/auth.py
@@ -1,81 +1,89 @@
 import sqlite3
 
 class DatabaseObject(object):
-  def __init__(self, data_file):
-    self.db = sqlite3.connect(data_file, check_same_thread=False)
-    self.data_file = data_file
 
-  def write(self, query, values=None):
-    cursor = self.db.cursor()
-    if values is not None:
-      cursor.execute(query, list(values))
-    else:
-      cursor.execute(query)
-    self.db.commit()
-    return cursor
 
-  def read(self, query, values=None):
-    cursor = self.db.cursor()
-    if values is not None:
-      cursor.execute(query, list(values))
-    else:
-      cursor.execute(query)
-    return cursor
+    def __init__(self, data_file):
+        self.db = sqlite3.connect(data_file, check_same_thread=False)
+        self.data_file = data_file
 
-  def drop_table(self, name):
-    cursor = self.db.cursor()
-    query = 'DROP TABLE %s' % name
-    cursor.execute(query)
-    self.db.commit()
-    cursor.close()
+    def write(self, query, values=None):
+        cursor = self.db.cursor()
+        if values is not None:
+            cursor.execute(query, list(values))
+        else:
+            cursor.execute(query)
+        self.db.commit()
+        return cursor
+
+    def read(self, query, values=None):
+        cursor = self.db.cursor()
+        if values is not None:
+            cursor.execute(query, list(values))
+        else:
+            cursor.execute(query)
+        return cursor
+
+    def drop_table(self, name):
+        cursor = self.db.cursor()
+        query = 'DROP TABLE %s' % name
+        cursor.execute(query)
+        self.db.commit()
+        cursor.close()
 
 
 class Table(DatabaseObject):
-  def __init__(self, data_file, table_name, values):
-    super(Table, self).__init__(data_file)
-    cursor = self.db.cursor()
-    query = 'CREATE TABLE IF NOT EXISTS %s%s' % (table_name, values)
-    cursor.execute(query)
-    self.db.commit()
-    cursor.close()
-    self.table_name = table_name
 
-  def insert(self, *args):
-    values = ','.join(['?' for l in args])
-    query = 'INSERT INTO %s VALUES(%s)' % (self.table_name, values)
-    cursor = self.write(query, args)
-    cursor.close()
 
-  def delete(self, **kwargs):
-    conds = ' and '.join(['%s=?' % k for k in kwargs])
-    subs = [kwargs[k] for k in kwargs]
-    query = 'DELETE FROM %s where %s' % (self.table_name, conds)
-    cursor = self.write(query, subs)
-    cursor.close()
+    def __init__(self, data_file, table_name, values):
+        super(Table, self).__init__(data_file)
+        cursor = self.db.cursor()
+        query = 'CREATE TABLE IF NOT EXISTS %s%s' % (table_name, values)
+        cursor.execute(query)
+        self.db.commit()
+        cursor.close()
+        self.table_name = table_name
 
-  def delete_all(self):
-    query = 'DELETE from %s' % self.table_name
-    cursor = self.write(query)
-    cursor.close()
+    def insert(self, *args):
+        values = ','.join(['?' for l in args])
+        query = 'INSERT INTO %s VALUES(%s)' % (self.table_name, values)
+        cursor = self.write(query, args)
+        cursor.close()
 
-  def drop():
-    self.drop_table(self.table_name)
+    def delete(self, **kwargs):
+        conds = ' and '.join(['%s=?' % k for k in kwargs])
+        subs = [kwargs[k] for k in kwargs]
+        query = 'DELETE FROM %s where %s' % (self.table_name, conds)
+        cursor = self.write(query, subs)
+        cursor.close()
+
+    def delete_all(self):
+        query = 'DELETE from %s' % self.table_name
+        cursor = self.write(query)
+        cursor.close()
+
+    def drop():
+        self.drop_table(self.table_name)
 
 
 class User(Table):
-  def __init__(self, data_file):
-    super(User, self).__init__(data_file, 'users', '(username TEXT, password TEXT)')
 
-  def exists(self, username):
-    query = 'SELECT username FROM users WHERE username=?'
-    cursor = self.read(query, [username])
-    results = cursor.fetchall()
-    cursor.close()
-    return len(results) > 0
 
-  def authenticate(self, username, password):
-    query = 'SELECT password FROM users WHERE username=? and password=?'
-    cursor = self.read(query, [username, password])
-    results = cursor.fetchall()
-    cursor.close()
-    return len(results) > 0
+    def __init__(self, data_file):
+        super(User, self).__init__(data_file, 'users', 
+                                   '(username TEXT, password TEXT)')
+
+    def exists(self, username):
+        query = 'SELECT username FROM users WHERE username=?'
+        cursor = self.read(query, [username])
+        results = cursor.fetchall()
+        cursor.close()
+        return len(results) > 0
+
+    def authenticate(self, username, password):
+        query = '''SELECT password FROM users WHERE username=? 
+                and password=?'''
+        cursor = self.read(query, [username, password])
+        results = cursor.fetchall()
+        cursor.close()
+        return len(results) > 0
