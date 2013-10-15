afc639e3993c5f1b062a2c65d57844868779e484
Jason <jason@ubuntu.(none)>
2013-10-15 00:58:44 -0400

    Original Comment

--- login/6/zhu_julie_zhen_jason/app.py
+++ login/6/zhu_julie_zhen_jason/app.py
@@ -10,21 +10,38 @@ import auth
 
 @app.route("/")
 def index():
-    #if ("username" in session):
-    #return render_template("index.html",username=session["username"])
-    #else:
-    return redirect("/login")
+    if ("username" in session):
+        return render_template("index.html",username=session["username"])
+    else:
+        return redirect("/login")
 
 @app.route("/login",methods=["GET","POST"])
 def login():
     if (request.method=="GET"):
         return render_template("login.html")
-
-    Username=request.form["Username"].encode("ascii","ignore")
-    Password=request.form["Password"].encode("ascii","ignore")
+    else:
+        if auth.log(str(request.form["username"]),str(request.form["password"])):
+            Username=request.form["Username"].encode("ascii","ignore")
+            Password=request.form["Password"].encode("ascii","ignore")
+            return redirect("/")
 
 @app.route("/register",methods=["GET","POST"])
 def register():
     if (request.method=="GET"):
         return render_template("register.html")
+    elif (request.form['button']=="Cancel"):
+        return redirect("/login")
+    else:
+        if request.form["password"]==request.form["cpassword"]:
+            if auth.addUser(str(request.form["username"]),str(request.form["password"]) ):
+                return redirect("/login")
+
+@app.route("/logout",methods=["GET","POST"])
+def logout():
+    session.pop("username",None)
+    return redirect("/")
+
+if __name__=="__main__":
+    app.debug=True
+    app.run(host='0.0.0.0',port=5000)
     
