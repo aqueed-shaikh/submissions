2e72cb12350dd0c035cd15c00a80972bed1dd0c1
Christopher Burke <cburke496@gmail.com>
2013-10-18 13:14:16 -0400

    Start Mongo things

--- login/7/burke_christopher_lin_hanson/auth.py
+++ login/7/burke_christopher_lin_hanson/auth.py
@@ -1,21 +1,32 @@
 import sqlite3
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.newdb
+db.newcollection
 
 
 def adduser(username, password):
-    connection = sqlite3.connect('test.db')
-    q = "select * from logins where username='%s'"%(username)
-    cursor = connection.execute(q)
-    results = []
-    for line in cursor:
-        results.append(line)
+    #connection = sqlite3.connect('test.db')
+    #q = "select * from logins where username='%s'"%(username)
+    #cursor = connection.execute(q)
+    #results = []
+    #for line in cursor:
+    #    results.append(line)
 
     
-    if len(results) == 0 :
-        q = "insert into logins values ('%s','%s')"%(username,password)
-        connection.execute(q)
-        connection.commit()
+    #if len(results) == 0 :
+    #    q = "insert into logins values ('%s','%s')"%(username,password)
+    #    connection.execute(q)
+    #    connection.commit()
+    #    return True
+    #else :
+    #    return False
+    copy = [x for x in db.newcollection.find({'username':username})]
+    if len(copy) == 0:
+        db.newcollection.insert({'username':username,'password':password})
         return True
-    else :
+    else:
         return False
 
 
