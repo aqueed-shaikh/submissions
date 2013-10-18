2e78a68d4c03e42aa7ce95e5c34b4799ee4ccee5
Ben Kurtovic <ben.kurtovic@verizon.net>
2013-10-18 12:29:52 -0400

    Mongo

--- login/6/kurtovic_benjamin_ye_tyrone/utils.py
+++ login/6/kurtovic_benjamin_ye_tyrone/utils.py
@@ -1,32 +1,24 @@
-import sqlite3
+from pymongo import MongoClient
 
 def connect():
-    conn = sqlite3.connect("login.db")
-    try:
-        conn.execute("SELECT 1 FROM login")
-    except:
-        conn.execute("CREATE TABLE login (username TEXT, password TEXT)")
+    conn = MongoClient()
     return conn
 
 def login(username, password):
     if not username or not password:
         return "missing"
     with connect() as conn:
-        query = "SELECT username, password FROM login WHERE username = ?"
-        r = conn.execute(query, [username])
-        results = r.fetchall()
-        if not results:
+        r = conn.login.login.findone({"username": username}, fields={"_id": False})
+        if not r:
             return "no-user"
-        if password != results[0][1]:
+        if password != r["password"]:
             return "incorrect"
 
 def register(username, password):
     if not username or not password:
         return "missing"
     with connect() as conn:
-        query = "SELECT username FROM login WHERE username = ?"
-        r = conn.execute(query, [username])
-        results = r.fetchall()
-        if results:
+        r = conn.login.login.findone({"username": username}, fields={"_id": False})
+        if r:
             return "exists"
-        conn.execute("INSERT INTO login VALUES (?, ?)", [username, password])
+        conn.login.login.insert({"username": username, "password": password})
