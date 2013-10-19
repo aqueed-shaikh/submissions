21124d24f63448f4f67d3eb88cca78443702f207
Aaron Coppa <aacoppa@gmail.com>
2013-10-19 16:39:15 -0400

    Switched To MongoDB

--- login/6/coppa_aaron_ong_timothy/app.py
+++ login/6/coppa_aaron_ong_timothy/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import session,url_for, request, redirect, render_template
-import sqlite3,utils
+import utils
 
 app = Flask(__name__)
 app.secret_key="ijasdb012fbrfasdffb0vbevs"
@@ -17,8 +17,8 @@ def login():
     if request.method == "GET":
         return render_template("login.html")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form["username"].encode("ascii", "ignore")
+        password = request.form["password"].encode("ascii", "ignore")
         if (utils.loginauth(username,password)):
             session["username"] = username
             return redirect(url_for("home"))
@@ -30,8 +30,8 @@ def register():
     if request.method == "GET":
         return render_template("register.html")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
+        username = request.form["username"].encode("ascii", "ignore")
+        password = request.form["password"].encode("ascii", "ignore")
         if (utils.regisauth(username,password)):
             session["username"] = username
             return redirect(url_for("home"))
