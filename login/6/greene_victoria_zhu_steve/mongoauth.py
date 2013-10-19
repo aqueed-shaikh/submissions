5b2a3eb990c4b3638206567abbd691db21174c58
Steve Zhu <st.zhu1@gmail.com>
2013-10-19 12:59:13 -0400

    One app.py + auth files for each system(shelves/sql/mongo)

---
+++ login/6/greene_victoria_zhu_steve/mongoauth.py
@@ -0,0 +1,22 @@
+import pymongo
+
+def init_auth(app):
+	pass
+
+def create_db():
+	pass
+
+def get_users():
+	pass
+
+def get_users_as_list():
+	pass
+
+def create_user(username, password):
+	pass
+
+def username_exists(username):
+	pass
+
+def validate_user(username, password):
+	pass
