2fc22088aff227ad428dfaccef09893471bba522
Derek Tang <derek.tang.27@gmail.com>
2013-10-19 22:50:41 -0400

    period 6

--- login/6/chen_simon_tang_derek/app.py
+++ login/6/chen_simon_tang_derek/app.py
@@ -1,6 +1,7 @@
 #Made by Simon Chen and Derek Tang
 from flask import Flask
 from flask import session, url_for, request, redirect, render_template
+from pymongo import MongoClient
 import sqlite3
 import auth
 
