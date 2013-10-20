554c48de9c2332f8d7b60fcd6d0ade3b77664582
Jasper Lu <repsajrules@gmail.com>
2013-10-20 16:39:08 -0700

    mongo login homework

--- login/6/Cahn_David_Lu_Jasper/loginMongo.py
+++ login/6/Cahn_David_Lu_Jasper/loginMongo.py
@@ -1,40 +1,20 @@
 from flask import Flask
 from flask.ext import shelve
 from flask import session,url_for,request,redirect,render_template
+from pymongo import MongoClient
+import utils
 
 app = Flask(__name__)
-app.secret_key="mysecretkey"
-app.config['SHELVE_FILENAME'] = 'my_users.db'
-shelve.init_app(app)
+app.secret_key = 'hi'
+blah = MongoClient()
+db = blah.test
 
 @app.route("/")
 def home():
-    if 'count' in session:
-        return redirect("/welcome")
-    else:
-        return redirect("/login")
-
-@app.route("/count")
-def count():
-    try:
-        c = session['count']
-    except:
-        c=0
-    c=c+1
-    session['count']=c
-    page="""
-    <h1>The count is: %d</h1>
-    <a href="/count">count</a>
-    """
-    return page%(c)
-
-@app.route("/welcome")
-def welcome ():    
-    return "<h1> Welcome to our website! </h1>"
+    return redirect("/login")
 
 @app.route("/login",methods=['GET','POST'])
 def login():
-    my_users =  shelve.get_shelve('c')
     if request.method=="GET":
 	return render_template("login.html")
     else:
@@ -42,17 +22,17 @@ def login():
 	if button == 'Login':
             username = request.form['username'].encode ('ascii',"ignore")
 	    password = request.form['password'].encode ('ascii',"ignore")
-            if username in my_users and my_users [username]["password"] == password:
+            if utils.auth(username,password,db.login):
                 session['username'] = username
                 return redirect("/success")
             else:
                 return redirect ("/login")
+                print 'login attempt failed. Try again.'
 	else:
 	    return redirect("/register")
         
 @app.route("/register",methods = ["GET","POST"])
 def register():
-    my_users = shelve.get_shelve('c')
     if request.method=="GET":
 	return render_template("register.html")
     else:
@@ -60,8 +40,8 @@ def register():
 	if button == "Submit":
 	    username = request.form['username'].encode ('ascii',"ignore")
 	    password = request.form['password'].encode ('ascii',"ignore")
-            if not username in my_users:
-                my_users[username]= {'password': password}
+            if not utils.auth(username,password,db.login):
+		utils.addUser(username,password,db.login)
                 print "Account Created"
                 return redirect("/login")
             else:
@@ -70,12 +50,10 @@ def register():
 
 @app.route("/success")
 def success():
-    session['count'] = 1
     return "<h1> You have successfully logged in!</h1>"
 
 @app.route("/logout")
 def logout():
-    session.pop('count',None)
     return redirect(url_for('home'))
 
 if __name__=="__main__":
--- login/6/Cahn_David_Lu_Jasper/utils.py
+++ login/6/Cahn_David_Lu_Jasper/utils.py
@@ -1,36 +1,7 @@
-import sqlite3
+from pymongo import MongoClient
 
-def userNameExist (sql_database,username):
-    ans = 0
-    db = sqlite3.connect ('SQL_Users')
-    dbcheck = db.execute ('select * FROM users WHERE username = ?', (username,))
-    if len (dbcheck.fetchall()) != 0:
-        ans = 1
+def auth (user, passwd, coll):
+    return [x for x in coll.find({'username': user, 'password': passwd})] != [] 
 
-    if ans == 0:
-        print 'user does not exist'
-    else:
-        print 'user exists'
-    return ans;
-
-def authenticate (sql_database, username, password):
-    ans = 0
-    if (userNameExist (sql_database,username)== 1):
-        db = sqlite3.connect ('SQL_Users')
-        dbcheck = db.execute ('select * FROM users WHERE username = ? and password = ?', (username,password))
-
-        if len(dbcheck.fetchall()) != 0:
-            ans = 1
-    print 'authentication compelete'
-    return ans
-
-def addUser (sql_database, username, password):
-    db = sqlite3.connect ('SQL_Users')
-    db.execute('insert into users(username, password) values(?, ?)', (username, password))
-    db.commit()
-    print 'user added'
-
-
-
-
-    
+def addUser (user, passwd, coll):
+    coll.insert({'username': user, 'password': passwd})
