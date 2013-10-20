3bc4d97e711b44c506df28ab9aa82de8f87bb38f
Andrew-Zarenberg <andzz42@gmail.com>
2013-10-20 17:47:34 -0400

    login with mongo

--- login/6/zarenberg_andrew_jing_lin/auth.py
+++ login/6/zarenberg_andrew_jing_lin/auth.py
@@ -1,29 +1,12 @@
-import sqlite3
-c = sqlite3.connect("auth.db",check_same_thread=False)
+from pymongo import MongoClient
+c = MongoClient()
 def authorize(username, password):
-    q = "SELECT * FROM users WHERE `username`='"+username+"' and `password`='"+password+"'"
-    r = c.execute(q)
-    for line in r:
-        return True
-    return False
+    return len(list(c.users.Collections.find({'username':username, 'password':password}))) == 1
 def userExists(username):
-    q = "SELECT * FROM users WHERE `username`='"+username+"'"
-    r = c.execute(q)
-    for line in r:
-        return True
-    return False                                                                                                       
+    return len(list(c.users.Collections.find({'username':username}))) == 1
 def createUser(username, password):
     if not userExists(username):
-    
-        q = "SELECT `id` FROM users"
-        r = c.execute(q)
-        count = 1
-        for line in r:
-            count += 1
-    
-        q = "INSERT INTO users VALUES(%d,'%s','%s')"%(count,username,password)
-        c.execute(q)
+        c.users.Collections.insert({'username':username, 'password':password})
         return True
     else:
         return False
-
