8742283e320300db62b5c976d1efef98362649af
Andrew-Zarenberg <andzz42@gmail.com>
2013-10-20 18:22:04 -0400

    login with mongo

--- login/6/zarenberg_andrew_jing_lin/auth.py
+++ login/6/zarenberg_andrew_jing_lin/auth.py
@@ -1,12 +1,15 @@
 from pymongo import MongoClient
-c = MongoClient()
+c = MongoClient('db.stuycs.org')
+db=c.admin
+db.authenticate('softdev','softdev')
+db = c.pd6
 def authorize(username, password):
-    return len(list(c.users.Collections.find({'username':username, 'password':password}))) == 1
+    return len(list(db.Collections.find({'username':username, 'password':password}))) == 1
 def userExists(username):
-    return len(list(c.users.Collections.find({'username':username}))) == 1
+    return len(list(db.Collections.find({'username':username}))) == 1
 def createUser(username, password):
     if not userExists(username):
-        c.users.Collections.insert({'username':username, 'password':password})
+        db.Collections.insert({'username':username, 'password':password})
         return True
     else:
         return False
