9a8a6cacf13c0e591084d195aaa9cd60ec22db76
Jae Ahn <jaeahngenghiskhan@suns-imac.home>
2013-10-20 23:53:41 -0400

    jae ahn & sweyn authMongo

---
+++ login/6/ahn_jae_venderbush_sweyn/authMongo.py
@@ -0,0 +1,32 @@
+import pymongo
+from pymongo import MongoClient
+client = MongoClient('db.stuycs.org')
+c = client
+c.createcollection("info")
+#collection = logins.db 
+
+def checkUsername(username):
+    ans = False; 
+    if ((c.info.find_one({'username':username}), fields = {"_id": False}))
+        return ans;
+    else 
+        ans = true;
+        return ans;
+
+def checkLogin(username, password): 
+    ans = False;
+    if checkUsername(username):
+        if ((c.info.find({'username': username} , {"password" : password}))):
+            ans = True;
+            return ans;
+        else:
+            return ans;
+
+def addLogin(username, password): 
+    if checkUsername(username) == False:
+        c.info.insert ({'username':username}, {'password':password})
+    else: 
+        print 'try another username'
+    
+        
+        
