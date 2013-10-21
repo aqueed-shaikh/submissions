424a42811a32137ffe70bbb706bddb6761117ee1
Marlena <marlena.lui@gmail.com>
2013-10-20 20:24:53 -0400

    woosh

--- login/6/lui_marlena_luo_jason/app.py
+++ login/6/lui_marlena_luo_jason/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import session, url_for, request, redirect, render_template
-import sqlite3
-import auth
+#import sqlite3
+#import auth
+import utils
 
 app = Flask(__name__)
 app.secret_key="marleyandme"
@@ -20,38 +21,46 @@ def login():
     else:
         username = request.form["username"].encode("ascii", "ignore")
         password = request.form["password"].encode("ascii", "ignore")
-        if (auth.checkuser(username, password)):
-            return "Success!"
-        else:
-            return "Please register!"
+        if (utils.checkuser(username, password)) == 0:
+            session["username"] = username
+            return redirect("/success")
+        if (utils.checkuser(username, password)) == 1:
+            print "username does not exist. please register."
+            return redirect("/register")
+        if (utils.checkuser(username, password)) == 2:
+            print "password does not match username. please try again."
+            return redirect("/login")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
         return render_template("register.html")
     else:
-        username = request.form["username"].encode("ascii", "ignore")
-        password = request.form["password"].encode("ascii", "ignore")
-<<<<<<< HEAD
-        users = shelve.get_shelve()
-        if users.has_key(username):
-            return render_template("register.html")
-        users[username] = password
-        session["username"] = username
-        return redirect(url_for("home"))
+        button = request.form["button"]
+        if button == "register":
+            username = request.form["username"].encode("ascii", "ignore")
+            password = request.form["password"].encode("ascii", "ignore")
+            if not utils.adduser(username, password):
+                print "this username already exists. please pick another."
+                return redirect("/register")
+            session["username"] = username
+            return redirect("/success")
                     
-=======
-        if (not auth.checkuser(username, password)):
-            auth.adduser(username, password)
-            return "You have created an account"
-        else:
-            return "User already exists"
+        #if (not auth.checkuser(username, password)):
+         #   auth.adduser(username, password)
+          #  return "You have created an account"
+       # else:
+        #    return "User already exists"
 
->>>>>>> 8742283e320300db62b5c976d1efef98362649af
+
 @app.route("/reset", methods = ['GET', 'POST'])
 def reset():
     session.pop("username", None)
     return redirect(url_for("login"))
+
+@app.route("/success")
+def success():
+    return "<h1> you have logged in successfully. congratz. </h1>"
 
 if __name__ == "__main__":
     app.debug = True
