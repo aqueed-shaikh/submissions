c1d130cb8f567b5a175de6c20f83601650361187
Benjamin Attal <attal.benjamin@gmail.com>
2013-10-20 23:27:05 -0400

    Finishing touches on login: fix erros in auth.py, add confirming for passwords, clean up templates

--- login/7/attal_benjamin_lin_michele/login2/auth.py
+++ login/7/attal_benjamin_lin_michele/login2/auth.py
@@ -5,18 +5,18 @@ db = client['logins']
 
 
 def insert(username, password):
-    db.logins.insert({'username': username, 'password': password})
+    db.users.insert({'username': username, 'password': password})
 
 
 def exists(username):
-    user = db.logins.find({'username': username}, fields={'_id': False})
+    user = db.users.find({'username': username}, fields={'_id': False})
     return len([u for u in user]) > 0
 
 
 def authenticate(username, password):
-    user = db.logins.find({'username': username, 'password': password},
+    user = db.users.find({'username': username, 'password': password},
         fields={'_id': False})
     return len([u for u in user]) > 0
 
 def change(username, newpass):
-    db.login.update({"username":username}, {$set: {"password":newpass}})
+    db.users.update({'username': username}, {'$set': {'password': newpass}})
