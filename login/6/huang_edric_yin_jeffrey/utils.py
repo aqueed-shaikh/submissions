0cc52c79a56a33231b00797de927dbe6b6efeea9
Jeffrey Yin <jyin96@gmail.com>
2013-10-21 05:55:31 -0400

    Program now runs on mongo

--- login/6/huang_edric_yin_jeffrey/utils.py
+++ login/6/huang_edric_yin_jeffrey/utils.py
@@ -1,46 +1,38 @@
-import sqlite3
-import sys
-
-def reset():
-    connect = sqlite3.connect('userdata.db')
-    cur=connect.cursor()
-    cur.execute("DROP TABLE IF EXISTS Users")
-    cur.execute("CREATE TABLE Users(Username TEXT, Password TEXT)")
+import pymongo
+from pymongo import MongoClient
 
 def authenticate(username, password):
-    connect = sqlite3.connect('userdata.db')
-    cur = connect.cursor()
-    with connect:
-        cur.execute("SELECT Password from Users WHERE Username = '%s'" %username)
-        row = cur.fetchall()
-        if row == []: #username is not found
-            return 1
-        elif row[0][0] == password: #correct login info
-            return 3
-        else: #wrong password
-            return 2
+    client = MongoClient()
+    db = client['userdata']
+    user = db.Users.find({ 'Username' : username })
+    if user.count () == 0: #username is not found
+        print "Username not found! Register first"
+        return 1
+    elif list(user)[0]["Password"] == password:
+        print "This was a triumph."
+        return 3
+    else: #wrong password
+        print "Wrong password."
+        return 2
 
 def register(username, password):
 #sanitize inputs here
-    
-    connect = sqlite3.connect('userdata.db')
-    cur=connect.cursor()
     if len(username) < 4:
         return 0
     if len(password) < 6:
         return 1
-    with connect:
-        cur.execute("SELECT * from Users WHERE Username = '%s'" %username)
-        row = cur.fetchall()
-        print row
-        if row != []:
-            return 2
-        else: 
-            cur.execute("INSERT INTO Users VALUES ('"+username +"','"+password+"');")
-            connect.commit()
-
+    
+    client = MongoClient()
+    db = client['userdata']
+    user = db.Users.find({ 'Username':username})
+    print user.count()
+    if user.count() != 0:
+        print "Username already exists"
+        return 2
+    else: 
+        print "Success!"
+        db.Users.insert({'Username' : username, 'Password' : password})
+        
 if __name__ == "__main__":
     register("jyin", "password")
-    register("kevin", "password")
-    register("steve", "awkwardcat")
     authenticate("jyin", "password")
