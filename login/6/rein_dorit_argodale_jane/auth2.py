40c7262636664491ca1f572f79cba8d113187302
dsrein <dsrein@me.com>
2013-10-20 21:06:43 -0400

    Auth-Mongo

--- login/6/rein_dorit_argodale_jane/auth2.py
+++ login/6/rein_dorit_argodale_jane/auth2.py
@@ -4,7 +4,7 @@ def usedUsername(your_username):
     ans = False
     connection = MongoClient()
     db = connection['users']
-    if(db.users.find({"username": "your_username"})
+    if(db.users.find({"username": your_username})
         ans = True
     return ans
 
@@ -12,7 +12,7 @@ def check(your_username, your_password):
     ans = False
     connection = MongoClient()
     db = connection['users']
-    list = list(db.users.find({"username": "your_username", "password": "your_password"}))
+    list = list(db.users.find({"username": your_username, "password": your_password}))
     if(list.len == 1)
        return ans
 
