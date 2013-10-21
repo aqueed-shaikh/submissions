e871f2b6be90d0a9de0bc1a8d557c61c6f9860d6
dsrein <dsrein@me.com>
2013-10-20 21:05:51 -0400

    Auth-Mongo

--- login/6/rein_dorit_argodale_jane/auth2.py
+++ login/6/rein_dorit_argodale_jane/auth2.py
@@ -8,18 +8,14 @@ def usedUsername(your_username):
         ans = True
     return ans
 
-def check(sql_database,username,password):
+def check(your_username, your_password):
     ans = False
-    if (usedUsername(sql_database,username)):
-        logins = sqlite3.connect('SQL_users')
-        checklogins = logins.execute('select * FROM users WHERE username = ? and password = ?', (username, password))
-        if len(checklogins.fetchall()) != 0:
-            ans = True
-    return ans
+    connection = MongoClient()
+    db = connection['users']
+    list = list(db.users.find({"username": "your_username", "password": "your_password"}))
+    if(list.len == 1)
+       return ans
 
-def add(sql_database,username,password):
-    logins = sqlite3.connect('SQL_users')
-    logins.execute('insert into users(username,password) values(?,?)', (username,password))
-    logins.commit()
+
     
         
