1f11bdc31352319a762dbfd0d2056fff42e2a940
Judy Mai <judymai@ubuntu.(none)>
2013-10-21 02:07:21 -0400

    Add app.py

--- login/7/mai_judy_ezratty_maia/mongoDB/app.py
+++ login/7/mai_judy_ezratty_maia/mongoDB/app.py
@@ -3,12 +3,10 @@
 #Judy Mai & Maia Ezratty 
 
 from flask import Flask, request, url_for, render_template, session, redirect
-from flask.ext import shelve
+from auth import adduser, authenticate, changepw 
 
 app = Flask(__name__)
-app.config["SHELVE_FILENAME"] = "shelve.db"
 app.secret_key = "my secret key"
-shelve.init_app(app)
 
 
 @app.route("/", methods = ["GET", "POST"])
@@ -16,7 +14,7 @@ def home():
     if "username" in session:
         return "<h2>You are logged in.</h2>"
     else:
-        return redirect("login")
+        return redirect(url_for("login"))
 
 
 @app.route("/register", methods = ["GET", "POST"])
@@ -26,15 +24,13 @@ def register():
     elif request.method == "GET":
         return render_template("register.html", message = "")
     else: 
-        userlist = shelve.get_shelve()
         user = request.form["user"].encode("utf8")
         pw = request.form["pass"].encode("utf8")
-        if user in userlist:
-            return render_template("register.html", message = "Username already taken.")
+        if adduser(user, pw):
+            return redirect("login")
         else:
-            userlist[user]=pw
-            session["username"] = user
-            return redirect("home")
+	    return render_template("register.html", message = "Username already taken.")
+            
         
         
 #Make a login page that has a login button and a link to the register page. When you log in, 
@@ -46,18 +42,31 @@ def login():
     elif request.method == "GET":
         return render_template("login.html", message = "")
     else:
-        userlist = shelve.get_shelve()
         user = request.form["user"].encode("utf8")
         pw = request.form["pass"].encode("utf8")
         if user == "" or pw == "":
             return render_template("login.html", message = "Please try again.")
-        elif user not in userlist:
-            return render_template("login.html", message = "Username does not exist.")
-        elif userlist[user] == pw:
+        elif authenticate(user, pw):
             session["username"] = user
             return redirect("home")
         else:
             return render_template("login.html", message = "User/pass don't match.")
+
+
+@app.route("/changepw")
+def changepw():
+	if "username" in session:
+            if request.method == "GET":
+                return render_template("changepw.html", message = "")
+            else:
+                oldpw = request.form["oldpw"].encode("utf8")
+                newpw = request.form["newpw"].encode("utf8")
+                if changepw(session["username"], oldpw, newpw):
+                    return redirect("login")
+                else:
+                    return redirect("changepw.html", message = "Please try again.")	
+	else:
+            return redirect("login")		
 
 
 #Make a logout route that pops the username from the session.
