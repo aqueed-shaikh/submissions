e4d21797be061edc352dd399ca7a84f94632d607
Judy Mai <judymai@ubuntu.(none)>
2013-10-21 02:11:50 -0400

    Fix indent

--- login/7/mai_judy_ezratty_maia/mongoDB/auth.py
+++ login/7/mai_judy_ezratty_maia/mongoDB/auth.py
@@ -10,13 +10,11 @@ def adduser(username, password):
     db.users.insert({'username': username, 'password': password})
 
 def authenticate(username, password):
-    user = db.users.find({'username': username, 'password': password},
-        fields={'_id': False})
+    user = db.users.find({'username': username, 'password': password}, fields={'_id': False})
     return len([u for u in user]) > 0
 
 def changepw(username, oldpw, newpw):
-    user = db.users.find({'username': username, 'password': oldpw},
-        fields={'_id': False})
+    user = db.users.find({'username': username, 'password': oldpw}, fields={'_id': False})
     if len([u for u in users]) == 0:
         return false
     else:
