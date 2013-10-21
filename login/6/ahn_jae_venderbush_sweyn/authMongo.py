da443f6c5eea5d4f666be3dc26d54b2958546d2c
Sweyn Venderbush <sweyn3@yahoo.com>
2013-10-21 01:56:29 -0400

    Final version

--- login/6/ahn_jae_venderbush_sweyn/authMongo.py
+++ login/6/ahn_jae_venderbush_sweyn/authMongo.py
@@ -1,24 +1,19 @@
-import pymongo
 from pymongo import MongoClient
 
 def checkUsername(username):
-    client = MongoClient('db.stuycs.org')
+    client = MongoClient()
     db=client.admin
-    db.authenticate('softdev','softdev')
     ans = False; 
     if (db.info.find_one({'username':username})):
-        return ans;
-    else: 
         ans = True;
-        return ans;
+    return ans;
 
 def checkLogin(username, password): 
-    client = MongoClient('db.stuycs.org')
+    client = MongoClient()
     db=client.admin
-    db.authenticate('softdev','softdev')
     ans = False;
     if checkUsername(username):
-        if ((db.info.find({'username': username} , {"password" : password}))):
+        if ((db.info.find_one({'username': username} , {"password" : password}))):
             ans = True;
             return ans;
         else: 
@@ -27,32 +22,27 @@ def checkLogin(username, password):
         return ans;
 
 def addLogin(username, password): 
-    client = MongoClient('db.stuycs.org')
+    client = MongoClient()
     db=client.admin
-    db.authenticate('softdev','softdev')
     if not checkUsername(username) and not checkLogin(username, password):
-        db.info.insert ({'username':username}, {'password':password}, {'count': 0})
+        db.info.insert ({'username':username, 'password':password, 'count': 0})
 
-    
-        
         
 def incrementCount(username):
-    client = MongoClient('db.stuycs.org')
+    client = MongoClient()
     db=client.admin
-    db.authenticate('softdev','softdev')
     if checkUsername(username):
         account = db.info.find_one({'username':username})
         account['count'] = account['count'] + 1
         db.info.save(account)
 
 def getCount(username):
-    client = MongoClient('db.stuycs.org')
+    client = MongoClient()
     db=client.admin
-    db.authenticate('softdev','softdev')
     if checkUsername(username):
         account = db.info.find_one({'username':username})
         return account['count']
-        
+
     
         
     
