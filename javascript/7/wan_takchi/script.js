898c500c1c3283255f23c8dd5366eb1f30c811c9
takchiwan <ray325789@yahoo.com>
2013-12-04 20:56:37 -0500

    Update script.js

--- javascript/7/wan_takchi/script.js
+++ javascript/7/wan_takchi/script.js
@@ -1,10 +1,9 @@
 function changecolor(e){
-    var a = document.getElementsByTagName("li");
-    for(var x=0;x<a.length;x++){
-        if(a[x].style.color === "blue"){
-            a[x].style.color = "";
-            a[x+1].style.color = blue;
-            break;
+    var items = document.getElementsByTagName("li");
+    for(var x=0;x<items.length;x++){
+        if(items[x].style.color === "blue"){
+            items[x].style.color = "";
+            items[x+1].style.color = "blue";
         }
     }
 }
