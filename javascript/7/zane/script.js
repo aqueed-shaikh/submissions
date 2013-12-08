9daa86e3ce2e98d5a3dcf365faafd9499479bd0d
Zane Sterling <sterling.zane@gmail.com>
2013-12-08 16:36:02 -0500

    Add things.

--- javascript/7/zane/script.js
+++ javascript/7/zane/script.js
@@ -1,13 +1,75 @@
-var e = document.createElement("h1");
-e.innerHTML = "woop";
-e.style.position = "absolute";
-document.firstChild.appendChild(e);
+var GAME = {
 
-var mover = function mover() {
-	e.style.left = Math.random().toFixed(3) * 90 + "%";
-	e.style.top = Math.random().toFixed(3) * 90 + "%";
+	interval : 2000,
+	intervalDiff : 50,
+	count : 0
 
-	setTimeout(mover, 400);
 }
 
-mover();
+var setup = function() {
+
+	var b = document.createElement("button");
+	b.innerHTML = "for the lulz";
+	b.style.position = "absolute";
+	b.style.left = "45%";
+	b.style.top  = "45%";
+	b.addEventListener("click", firstClick);
+	document.firstChild.appendChild(b);
+
+	var points = document.createElement("div");
+	points.id = "points";
+	document.firstChild.appendChild(points);
+
+	GAME.b = b;
+}
+
+var main = function() {
+
+	moveAll();
+	setTimeout(main, GAME.interval);
+}
+
+var moveAll = function() {
+
+	move(GAME.b);
+	var points = document.getElementById("points").children;
+	for (var i=0; i<points.length; i++) {
+		move(points[i]);
+	}
+}
+
+var move = function(element) {
+
+	element.style.left = Math.random().toFixed(3) * 90 + "%";
+	element.style.top = Math.random().toFixed(3) * 90 + "%";
+}
+
+var score = function() {
+
+	var points = document.getElementById("points");
+	if (points.children.length > 0)
+		points.children[GAME.count-1].style.color = "black";
+
+	GAME.count++;
+
+	var e = document.createElement("h1");
+	e.innerHTML = GAME.count;
+	e.style.color = "red";
+	e.style.position = "absolute";
+	document.getElementById("points").appendChild(e);
+
+	GAME.interval -= GAME.intervalDiff;
+
+	moveAll();
+}
+
+var firstClick = function() {
+
+	var b = GAME.b;
+	b.removeEventListener('click', firstClick);
+	b.addEventListener('click', score);
+	score();
+	main();
+}
+
+setup();
