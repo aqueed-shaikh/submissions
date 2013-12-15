20ab09315c1dd779df7aefdd4e2003a9c1d792ee
aleung013 <aleung013@gmail.com>
2013-12-14 22:47:12 -0500

    Modify todo.js

--- faketodo/7/leung_alvin/todo.js
+++ faketodo/7/leung_alvin/todo.js
@@ -1,23 +1,25 @@
-var removeFromList = function(){
-    document.getElementById("done").removeChild(this);
-}
-var moveToDONE = function(){
+var main = function(){
     var donelist = document.getElementById("done");
-    donelist.appendChild(this);
-    this.addEventListener("click",removeFromList);
-}
-
-var moveToTODO = function(){
-    var textbox = document.getElementById("whattodo");
-    var input = textbox.value;
     var todolist = document.getElementById("todo");
-    var todoitem = document.createElement("li");
-    todoitem.appendChild(document.createTextNode(input));
-    todoitem.addEventListener("click",moveToDONE);
-    todolist.appendChild(todoitem);
-    textbox.value = "";
-}
-
-var submitbutton = document.getElementById("submit");
-submitbutton.addEventListener("click",moveToTODO);
-
+    
+    var removeFromList = function(){
+	donelist.removeChild(this);
+    }
+    var moveToDONE = function(){
+	donelist.appendChild(this);
+	this.addEventListener("click",removeFromList);
+    }
+    
+    var moveToTODO = function(){
+	var textbox = document.getElementById("whattodo");
+	var input = textbox.value;
+	var todoitem = document.createElement("li");
+	todoitem.appendChild(document.createTextNode(input));
+	todoitem.addEventListener("click",moveToDONE);
+	todolist.appendChild(todoitem);
+	textbox.value = "";
+    }
+    
+    var submitbutton = document.getElementById("submit");
+    submitbutton.addEventListener("click",moveToTODO);
+}();
