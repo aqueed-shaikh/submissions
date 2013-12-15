65095f3d015892a27a3bd0e522617676ce21e989
Jing Lin <Jinglin82@gmail.com>
2013-12-15 18:08:36 -0500

    "Working on FTD"

---
+++ faketodo/6/lin_jing/Javascript.js
@@ -0,0 +1,21 @@
+function addItem()
+{
+    var ToBeAdded = document.getElementById("text").value;
+    var Item = document.createElement("li");
+    Item.innerHTML = toBeAdded;
+    Item.addEventListener("click", PopToDone);
+    document.getElementById("todo").appendChild(Item);
+}
+
+function PopToDone() 
+{
+    var Item = document.getElementById("todo");
+    document.getElementById("done").appendChild(Item);
+    this.removeEventListener("click", PopToDone);
+    this.addEventListener("click", Delete);
+}
+
+function Delete() 
+{
+    document.getElementById("done").removeChild(this);
+}
